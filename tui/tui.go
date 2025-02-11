c24c59bb5c77839bd95edb7b120eacbb83bf2dfe
Jeremy L. <j.lugagne@tadaweb.com>
2025-02-11 11:34:39 +0100

    Enable tls and enable full uri in address flag

--- tui/tui.go
+++ tui/tui.go
@@ -2,6 +2,7 @@ package tui
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"os"
@@ -12,25 +13,37 @@ import (
 )
 
 type RedisOptions struct {
-	Address         string
-	Username        string
-	Password        string
-	DB              int
+	Address     string
+	Username    string
+	Password    string
+	DB          int
+	Tls         bool
+	TleInsecure bool
 }
 
-func CreateRedisClient(conn string, username string, password string, db int) (*redis.Client, error) {
+func CreateRedisClient(conn string, username string, password string, db int, enableTls, tlsInsecure bool) (*redis.Client, error) {
 	if conn == "" {
 		conn = "localhost:6379"
-	} else {
-		conn = strings.TrimPrefix(conn, "redis://")
 	}
-	redis := redis.NewClient(&redis.Options{
-		Addr:     conn,
-		Username: username,
-		Password: password,
-		DB:       db,
-	})
-	_, err := redis.Ping(context.Background()).Result()
+	if !strings.HasPrefix(conn, "redis://") && !strings.HasPrefix(conn, "rediss://") {
+		if enableTls {
+			conn = "rediss://" + conn
+		} else {
+			conn = "redis://" + conn
+		}
+	}
+	options, err := redis.ParseURL(conn)
+	if err != nil {
+		return nil, err
+	}
+	options.Username = username
+	options.Password = password
+	if enableTls && tlsInsecure {
+		options.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	redis := redis.NewClient(options)
+	_, err = redis.Ping(context.Background()).Result()
 	if err != nil {
 		return nil, err
 	}
@@ -43,15 +56,16 @@ func RunTUI(redis_opts RedisOptions, model_opts ModelOptions) {
 		redis_opts.Username,
 		redis_opts.Password,
 		redis_opts.DB,
+		redis_opts.Tls,
+		redis_opts.TleInsecure,
 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	p := tea.NewProgram(
 		InitialModel(
 			client,
-            model_opts,
+			model_opts,
 		),
 		tea.WithAltScreen(),
 	)
