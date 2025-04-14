f60eb51133ad305d7906a9da194007f3758d32a4
Alexander Demin <alexander@demin.ws>
2025-04-14 15:21:02 +0100

    add tls support
    - add tls support
    - apply "go fmt"
    - rename to conform go coding conventions
    - add defailed error messages

--- cmd/flags.go
+++ cmd/flags.go
@@ -3,39 +3,57 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
 
 	"github.com/mat2cc/redis_tui/tui"
 )
 
-func Run(app_version string) {
+func Run(appVersion string) {
 	version := flag.Bool("version", false, "Application version")
 
-	addressPtr := flag.String("address", "localhost:6379", "Redis server address")
-	usernamePtr := flag.String("username", "", "Redis username (optional)")
-	passwordPtr := flag.String("password", "", "Redis password (optional)")
-	dbPtr := flag.Int("db", 0, "Redis db (default 0)")
+	address := flag.String("address", "localhost:6379", "Redis server address")
+	username := flag.String("username", "", "Redis username (optional)")
+	password := flag.String("password", "", "Redis password (optional)")
+	db := flag.Int("db", 0, "Redis db (default 0)")
+	tls := flag.Bool("tls", false, "Use TLS (default false)")
 
 	scanSize := flag.Int64("scan-size", 1000, "Number of keys scanned at a time")
-	prettyPrintJson := flag.Bool("pp-json", true, "Pretty print JSON values")
+	prettyPrint := flag.Bool("pp-json", true, "Pretty print JSON values")
 	includeTypes := flag.Bool("include-types", true, "Include type values when querying keys. This includes a pipeline of batched TYPE commands for each key scanned, which may impact performace.")
 	delimiter := flag.String("delimiter", ":", "Delimiter for key names.")
 
+	verbose := flag.Bool("verbose", false, "Verbose output")
+
 	flag.Parse()
-    if *version {
-        fmt.Println(app_version)
-        return
-    }
+	if *version {
+		fmt.Println(appVersion)
+		return
+	}
+
+	if *verbose {
+		addr, err := url.Parse(*address)
+		if err != nil {
+			log.Fatal(fmt.Errorf("parse address: %w", err))
+		}
+		log.Println("address:", addr.Redacted())
+		log.Println("username:", *username)
+		log.Println("password:", (*password)[:6]+"..."+(*password)[len(*password)-6:])
+		log.Println("tls:", *tls)
+		log.Println("db:", *db)
+	}
 
 	tui.RunTUI(
 		tui.RedisOptions{
-			Address:  *addressPtr,
-			Username: *usernamePtr,
-			Password: *passwordPtr,
-			DB:       *dbPtr,
+			Address:  *address,
+			Username: *username,
+			Password: *password,
+			DB:       *db,
+			TLS:      *tls,
 		},
 		tui.ModelOptions{
 			ScanSize:        *scanSize,
-			PrettyPrintJson: *prettyPrintJson,
+			PrettyPrintJson: *prettyPrint,
 			IncludeTypes:    *includeTypes,
 			Delimiter:       *delimiter,
 		},
